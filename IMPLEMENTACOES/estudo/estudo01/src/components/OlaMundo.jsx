42f7f01f27d0ca98fd339fa5146e69a9228cdf31
jeffersoncarvalho <jeffersoncarvalho@gmail.com>
2020-01-15 17:49:23 -0300

    aula01

---
+++ IMPLEMENTACOES/estudo/estudo01/src/components/OlaMundo.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default (props) =>
+    <div>
+        <h1>Olá {props.nome}!</h1>
+    </div>
---
+++ IMPLEMENTACOES/estudo/estudo01/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import OlaMundo from './components/OlaMundo';
+const root = document.getElementById('root');
+
+ReactDOM.render(<OlaMundo nome='Jefferson'/>,root);
+
