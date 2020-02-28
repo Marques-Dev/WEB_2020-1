5babc92469493f591ec38da11a533b09b3771749
jeffersoncarvalho <jeffersoncarvalho@gmail.com>
2020-02-28 09:40:50 -0300

    aula si 01

--- IMPLEMENTACOES/aula/aula01/src/index.js
+++ IMPLEMENTACOES/aula/aula01/src/index.js
@@ -1,23 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+import OlaMundo from './components/OlaMundo'
 
-const root = document.getElementById('root');
+const root = document.getElementById('root')
+ReactDOM.render(<OlaMundo nome='Jefferson' sobrenome='de Carvalho'/>,root);
 
-let ul = document.createElement('ul');
+// const textoJsx = 
+// <ul>
+//     <li>1-)Superman</li>
+//     <li>2-)Bátima</li>
+//     <li>3-)Wonder Woman</li>
+//     <li>3-)Wonder Woman</li>
+// </ul> 
 
-let li1 = document.createElement('li');
-let texto1 =  document.createTextNode('1-)Superman');
-li1.appendChild(texto1);
-ul.appendChild(li1);
+// ReacDOM.render(textoJsx,root)
 
-let texto2 = document.createTextNode('2-)Batman');
-let li2 = document.createElement('li');
-li2.appendChild(texto2);
-ul.appendChild(li2);
+// const root = document.getElementById('root');
 
-let texto3 = document.createTextNode('3-)Wonder Woman');
-let li3 = document.createElement('li');
-li3.appendChild(texto3);
-ul.appendChild(li3);
+// let ul = document.createElement('ul');
 
-root.appendChild(ul);
+// let li1 = document.createElement('li');
+// let texto1 =  document.createTextNode('1-)Superman');
+// li1.appendChild(texto1);
+// ul.appendChild(li1);
+
+// let texto2 = document.createTextNode('2-)Batman');
+// let li2 = document.createElement('li');
+// li2.appendChild(texto2);
+// ul.appendChild(li2);
+
+// let texto3 = document.createTextNode('3-)Wonder Woman');
+// let li3 = document.createElement('li');
+// li3.appendChild(texto3);
+// ul.appendChild(li3);
+
+// root.appendChild(ul);
 
