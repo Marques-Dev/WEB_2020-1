fae81946a8de1166e3230908ad618032f2d6dafc
jeffersoncarvalho <jeffersoncarvalho@gmail.com>
2020-04-06 15:11:57 -0300

    aula 03 pdf

---
+++ IMPLEMENTACOES/estudo/crud/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Create from './components/Create';
+import Edit from './components/Edit';
+import Index from './components/Index';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to={'/'} className="navbar-brand">CRUD</Link>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link to={'/'} className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/create'} className="nav-link">Create</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/index'} className="nav-link">Index</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <h2>Projeto CRUD</h2> <br />
+          <Switch>
+            <Route exact path='/create' component={Create} />
+            <Route path='/edit/:id' component={Edit} />
+            <Route path='/index' component={Index} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
